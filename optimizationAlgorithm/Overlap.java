a067540c77dd3b217764f6e26caae5d21b58111a
AlvaroVazVel <a01207516.itesm.mx>
2018-09-26 17:05:32 -0500

    Overlaps

--- optimizationAlgorithm/Overlap.java
+++ optimizationAlgorithm/Overlap.java
@@ -1,45 +1,92 @@
 package optimizationAlgorithm;
 
+import java.util.Set;
+
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Node;
+
 import optimizationAlgorithm.*;
 
-class Overlap
-{
+class Overlap {
     public int overlap_node;
     public int route_node_1;
     public int route_node_2;
     public double probability;
-    
-    public Overlap(int o, int r1, int r2, double p)
-    {
+
+    public Overlap(int o, int r1, int r2, double p) {
         overlap_node = o;
         route_node_1 = r1;
-        route_node_1= r2;
+        route_node_1 = r2;
         probability = p;
     }
 }
 
-class setOverlap
-{
+class setOverlap {
 
     public Overlap overlaps[];
 
-    private float getCost(int id, Node node)
-    {
-        for(int x = 0; x < node.TEdgeSize; x++)
-        {
-            if(node.transitionEdges[x].dest == id)
-            {   
+    private float getCost(int id, Node node) {
+        for (int x = 0; x < node.TEdgeSize; x++) {
+            if (node.transitionEdges[x].dest == id) {
                 return node.transitionEdges[x].cost;
             }
         }
         return 0;
     }
-    
-    public float getProbability(Node node_1, Node node_2, Node transition_node)
-    {
+
+    public float getProbability(Node node_1, Node node_2, Node transition_node) {
         cost_1 = getCost(transition_node.id, node_1);
         cost_2 = getCost(transition_node.id, node_2);
-        prob = 1-(cost_1/(cost_1+cost_2));
+        prob = 1 - (cost_1 / (cost_1 + cost_2));
         return prob;
     }
+
+    public Overlap[] getOverlaps(Routes rutas, int initial_n[], Node nodes[]) {
+
+        Node idx, idy;
+        int overlapsSize = 0;
+        int actualOverlap = 0;
+        float prob;
+        for (int x = 0; x < rutas.routes.length; x++) {
+
+            for (int y = x + 1; y < rutas.routes.length; y++) {
+                Set<Node> OverlapA = new HashSet<Node>(rutas.routes[x].hash_Set);
+                Set<Node> OverlapB = new HashSet<Node>(rutas.routes[y].hash_Set);
+
+                OverlapA.retainAll(OverlapB);
+                overlapsSize += OverlapA.size();
+            }
+        }
+        overlaps = new Overlap[overlapsSize];
+        for (int x = 0; x < rutas.routes.length; x++) {
+            // System.out.print("Route id: " + rutas.routes[x].routeName + " : ");
+
+            for (int y = x + 1; y < rutas.routes.length; y++) {
+                Set<Node> OverlapA = new HashSet<Node>(rutas.routes[x].hash_Set);
+                Set<Node> OverlapB = new HashSet<Node>(rutas.routes[y].hash_Set);
+
+                OverlapA.retainAll(OverlapB);
+
+                if (OverlapA.size() > 0) {
+                    idx = getNode(initial_n[x], nodes);
+                    idy = getNode(initial_n[y], nodes);
+                    for (Node s : OverlapA) {
+                        prob = getProbability(idx, idy, s);
+                        Overlap ov = new Overlap(s.id, idx.id, idy.id, prob);
+                        overlaps[actualOverlap] = ov;
+                    } // System.out.println();
+                }
+            }
+        }
+
+    }
+
+    private Node getNode(int id, Node nodes[]) {
+        for (int x = 0; x < nodes.length; x++) {
+            if (nodes[x].id == id) {
+                return nodes[x];
+            }
+        }
+        return null;
+    }
+
 }
--- optimizationAlgorithm/mainPruebas.java
+++ optimizationAlgorithm/mainPruebas.java
@@ -1,66 +1,74 @@
 
 package optimizationAlgorithm;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import optimizationAlgorithm.*;
 
-public class mainPruebas
-{
-    public static void main(String[] args) 
-    {
-        int [] a = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49};
-        int [] b = {101, 102, 103,104,105};
+public class mainPruebas {
+    public static void main(String[] args) {
+        int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49 };
+        int[] b = { 101, 102, 103, 104, 105 };
         int c = 100;
 
         DatabaseConnection data = new DatabaseConnection();
         Node[] w = data.nodes_matrix(b, c, a);
 
-       /* for(int x = 0; x < w.length;x++)
-        {
-            for(int y = 0; y < w[x].TEdgeSize ;y++)
-            {
-                System.out.print(w[x].id+" ");
-                System.out.print(w[x].transitionEdges[y].dest+" ");
-                System.out.print(w[x].transitionEdges[y].cost+" ");
-                System.out.print(w[x].transitionEdges[y].dist_cost+" ");
-                System.out.print(w[x].transitionEdges[y].time_cost+" ");
-                System.out.println();
-                
-            }
-
-            if(w[x].destEdge!=null)
-            {
-                System.out.print(w[x].id+" ");
-                System.out.print(w[x].destEdge.dest+" ");
-                System.out.print(w[x].destEdge.cost+" ");
-                System.out.print(w[x].destEdge.dist_cost+" ");
-                System.out.print(w[x].destEdge.time_cost+" ");
-                System.out.println();
-            }
-
-            for(int y = 0; y < w[x].IEdgeSize ;y++)
-            {
-                System.out.print(w[x].id+" ");
-                System.out.print(w[x].initialEdges[y].dest+" ");
-                System.out.print(w[x].initialEdges[y].cost+" ");
-                System.out.print(w[x].initialEdges[y].dist_cost+" ");
-                System.out.print(w[x].initialEdges[y].time_cost+" ");
-                System.out.println();
-            }
-            
-            System.out.println("------------------------------");
-        }*/
+        /*
+         * for(int x = 0; x < w.length;x++) { for(int y = 0; y < w[x].TEdgeSize ;y++) {
+         * System.out.print(w[x].id+" ");
+         * System.out.print(w[x].transitionEdges[y].dest+" ");
+         * System.out.print(w[x].transitionEdges[y].cost+" ");
+         * System.out.print(w[x].transitionEdges[y].dist_cost+" ");
+         * System.out.print(w[x].transitionEdges[y].time_cost+" ");
+         * System.out.println();
+         * 
+         * }
+         * 
+         * if(w[x].destEdge!=null) { System.out.print(w[x].id+" ");
+         * System.out.print(w[x].destEdge.dest+" ");
+         * System.out.print(w[x].destEdge.cost+" ");
+         * System.out.print(w[x].destEdge.dist_cost+" ");
+         * System.out.print(w[x].destEdge.time_cost+" "); System.out.println(); }
+         * 
+         * for(int y = 0; y < w[x].IEdgeSize ;y++) { System.out.print(w[x].id+" ");
+         * System.out.print(w[x].initialEdges[y].dest+" ");
+         * System.out.print(w[x].initialEdges[y].cost+" ");
+         * System.out.print(w[x].initialEdges[y].dist_cost+" ");
+         * System.out.print(w[x].initialEdges[y].time_cost+" "); System.out.println(); }
+         * 
+         * System.out.println("------------------------------"); }
+         */
 
         Routes rutas = new Routes(b);
         rutas.nodes_route(b, a, c, w);
 
-        for(int x = 0; x < rutas.routes.length; x++)
-        {
-            System.out.print("Route id: "+rutas.routes[x].routeName+" : ");
-            for (Node s : rutas.routes[x].hash_Set) 
-            {
-                System.out.print(s.id+",");
+        //
+        int y;
+        for (int x = 0; x < rutas.routes.length; x++) {
+            // System.out.print("Route id: " + rutas.routes[x].routeName + " : ");
+
+            for (y = x + 1; y < rutas.routes.length; y++) {
+                Set<Node> OverlapA = new HashSet<Node>(rutas.routes[x].hash_Set);
+                Set<Node> OverlapB = new HashSet<Node>(rutas.routes[y].hash_Set);
+
+                OverlapA.retainAll(OverlapB);
+                if (OverlapA.size() > 0) {
+                    System.out.print("Overlap x:" + x + " y:" + y + " ");
+                    for (Node s : OverlapA) {
+                        System.out.print(s.id + ",");
+                    }
+                    System.out.println();
+                }
             }
-            System.out.println(); 
+            //
+
+            /**
+             * for (Node s : rutas.routes[x].hash_Set) { System.out.print(s.id + ","); }
+             **/
+
         }
     }
 }
