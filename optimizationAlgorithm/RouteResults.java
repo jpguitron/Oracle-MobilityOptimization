a4ebf638c44a28fcee97ba25fb5434d9d99f1a8d
Luis Guitron <legl_1995@hotmail.com>
2018-09-27 12:56:54 -0500

    Tests

--- optimizationAlgorithm/RouteResults.java
+++ optimizationAlgorithm/RouteResults.java
@@ -16,11 +16,11 @@ public class RouteResults
                      
         int routeID         = MobilityOptimization.startNodes[routeIndex];
         int current_node_id = routeID; 
-        for(int j=0; j<routeChromosome.length(); j++)                                            // Loop through all transition nodes for the current route
+        for(int j=0; j<routeChromosome.length(); j++)                                       // Loop through all transition nodes for the current route
         {
             boolean addCost  = true;                                                        // Boolean to determine if a cost should be added for this transition
-            int next_node_index = routeChromosome.getGene(j).getAllele();                        // Index of possible node to visit
-            int next_node_id = MobilityOptimization.routeMap[routeIndex][next_node_index];        // Get next_node_id from map
+            int next_node_index = routeChromosome.getGene(j).getAllele();                   // Index of possible node to visit
+            int next_node_id = MobilityOptimization.routeMap[routeIndex][next_node_index];  // Get next_node_id from map
             for (int k=0;k<MobilityOptimization.overlaps.length;k++)                        // Loop to check if current node has overlap
             {
                 Overlap c_overlap = MobilityOptimization.overlaps[k];
@@ -29,7 +29,8 @@ public class RouteResults
                     // If node1 and bit == 0  OR node2 and bit == 1 dont add any cost (its added in the other route)
                     if(routeID == c_overlap.route_node_1)
                     {
-                        if(ownGenotype.getChromosome().getGene().getBit())
+                        int ownGenotypeIndex = MobilityOptimization.ownMapping.get(c_overlap.overlap_node);
+                        if(ownGenotype.getChromosome().getGene(ownGenotypeIndex).getBit())
                             break;
 
                         else
@@ -40,7 +41,8 @@ public class RouteResults
                     }
                     else
                     {
-                        if(ownGenotype.getChromosome().getGene().getBit())
+                        int ownGenotypeIndex = MobilityOptimization.ownMapping.get(c_overlap.overlap_node);
+                        if(ownGenotype.getChromosome().getGene(ownGenotypeIndex).getBit())
                         {
                             addCost = false;
                             break;
